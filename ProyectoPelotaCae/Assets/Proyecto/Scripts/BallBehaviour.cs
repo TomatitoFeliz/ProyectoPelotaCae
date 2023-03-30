44c469a24542e7c7b760bd4d9b52cf34b208076e
TomatitoFeliz <>
2023-03-30 12:05:25 +0100

    BigUpdate

--- ProyectoPelotaCae/Assets/Proyecto/Scripts/BallBehaviour.cs
+++ ProyectoPelotaCae/Assets/Proyecto/Scripts/BallBehaviour.cs
@@ -1,37 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallBehaviour : MonoBehaviour
-{
+{ 
     Rigidbody ballrigidbody;
-    public float fuerza = 5;
+    [SerializeField] float fuerza = 5f;
+    TrailRenderer trail;
+    [SerializeField] float ultraspeed = 10.0f;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision != null)
-        {
-            ballrigidbody.velocity = Vector3.zero;
-            ballrigidbody.AddForce(Vector3.up * fuerza, ForceMode.Impulse);
-            Debug.Log("Chocó");
-        }
-    }
-
+    float lastSpeedY = 0.0f;
     private void Start()
     {
+        trail = GetComponent<TrailRenderer>();
         ballrigidbody = GetComponent<Rigidbody>();
+        trail.enabled = false;
     }
 
     private void Update()
     {
-        if (!Mathf.Approximately(ballrigidbody.velocity.y, 0.0f))
+        lastSpeedY = ballrigidbody.velocity.y;
+        if (lastSpeedY < -6f)
         {
-            //Debug.Log("YOU LOOSE");
+            trail.enabled = true;
         }
-
-        if (ballrigidbody.velocity.y > 2)
+        else
         {
-            //TrailRenderer()
+            trail.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Basico")
+        {
+            if (lastSpeedY < -10f)
+            {
+                Debug.Log("Destruye plataforma Basico");
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                ballrigidbody.velocity = Vector3.zero;
+                ballrigidbody.AddForce(Vector3.up * fuerza, ForceMode.Impulse);
+                Debug.Log("Rebota");
+            }
+        }else if (collision.gameObject.tag == "Mortal")
+        {
+            if (lastSpeedY < -10f)
+            {
+                Debug.Log("Destruye plataforma mortal");
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                Debug.Log("Muerte");
+                Time.timeScale = 0.0f;
+                SceneManager.LoadScene("LVL1");
+            }
         }
     }
 }
